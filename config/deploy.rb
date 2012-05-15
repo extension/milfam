f1b5702aeade95ff6e749976bc36ce5055a68165
sdnall <s.daniel.nall@gmail.com>
2012-05-15 14:34:50 -0400

    btsnocp

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ namespace :deploy do
   task :link_and_copy_configs, :roles => :app do
     run <<-CMD
     rm -rf #{release_path}/wp-config.php &&
-    ln -nfs /services/config/#{application}/wp-config.php #{release_path}/wp-config.php &&
+    ln -nfs /services/config/#{application}/wordpress/wp-config.php #{release_path}/wp-config.php &&
     ln -nfs /services/wordpress/militaryfamilies.extension.org/uploads #{release_path}/wp-content/uploads &&
     ln -nfs /services/config/#{application}/wordpress/robots.txt #{release_path}/robots.txt
     CMD
