0c766219bf1a63f179b8fe0dd864a304c6e2f1d6
Jason Adam Young <jayoung@extension.org>
2016-10-06 08:30:20 -0400

    BTSNOCAP and not saving after, sigh

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -3,8 +3,3 @@ set :branch, 'master'
 set :vhost, 'militaryfamilies.extension.org'
 set :deploy_server, 'militaryfamilies.aws.extension.org'
 server deploy_server, :app, :web, :db, :primary => true
-
-set :deploy_to, "/services/search/"
-set :vhost, 'search.extension.org'
-set :deploy_server, 'search.aws.extension.org'
-server deploy_server, :app, :web, :db, :primary => true
