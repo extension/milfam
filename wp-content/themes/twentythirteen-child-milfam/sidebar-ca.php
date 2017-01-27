0c3e04135ffafc65ca56243bd48647f8c0f87ddd
Ben MacNeill <ben.macneill@gmail.com>
2017-01-27 10:40:16 -0500

    Remove the hardcoded shortcode

--- wp-content/themes/twentythirteen-child-milfam/sidebar-ca.php
+++ wp-content/themes/twentythirteen-child-milfam/sidebar-ca.php
@@ -2,11 +2,6 @@
 	<div id="tertiary" class="sidebar-container" role="complementary">
 		<div class="sidebar-inner">
 			<div class="widget-area">
-
-        <!-- check for a value set in the "Set Learn Tags" plugin -->
-        <?php $meta_value = get_post_meta( get_the_ID(), 'meta-text', true ); ?>
-
-        <?php echo do_shortcode("[learn_widget key='exlw-ca295c9d' tags='" . $meta_value . "' limit=3 match_all_tags=true]"); ?>
 				<?php dynamic_sidebar( 'sidebar-ca' ); ?>
 			</div><!-- .widget-area -->
 		</div><!-- .sidebar-inner -->
