5fa062e2ed3f3f705410423090357587f7c41315
Ben MacNeill <ben.macneill@gmail.com>
2017-03-23 08:30:55 -0400

    Incorporate search into header

--- wp-content/themes/twentyseventeen-child-milfam/header.php
+++ wp-content/themes/twentyseventeen-child-milfam/header.php
@@ -47,17 +47,31 @@
 
       <?php if ( has_nav_menu( 'top' ) ) : ?>
   			<div class="navigation-top">
-  				
+          <div class="navigation-top-menu">
             <nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php _e( 'Top Menu', 'twentyseventeen' ); ?>">
             	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false"><?php echo twentyseventeen_get_svg( array( 'icon' => 'bars' ) ); echo twentyseventeen_get_svg( array( 'icon' => 'close' ) ); _e( 'Menu', 'twentyseventeen' ); ?></button>
+              <!-- <div id="amobile-nav"> -->
             	<?php wp_nav_menu( array(
             		'theme_location' => 'top',
             		'menu_id'        => 'top-menu',
             	) ); ?>
+              <!-- </div> -->
             </nav><!-- #site-navigation -->
+          </div>
+          <div class="navigation-top-search">
+            <?php $unique_id = esc_attr( uniqid( 'search-form-' ) ); ?>
 
+            <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+            	<label for="<?php echo $unique_id; ?>">
+            		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'twentyseventeen' ); ?></span>
+            	</label>
+            	<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'twentyseventeen' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+            	<button type="submit" class="search-submit"><?php echo twentyseventeen_get_svg( array( 'icon' => 'search' ) ); ?><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'twentyseventeen' ); ?></span></button>
+            </form>
+          </div>
 
   			</div><!-- .navigation-top -->
+
   		<?php endif; ?>
 
       </div><!-- .wrap -->
