4390e96ca86b628e4c32c208f3c237682d5b8e43
Ben MacNeill <ben.macneill@gmail.com>
2017-06-29 11:36:59 -0400

    fix image in facilitators template

--- wp-content/themes/twentyseventeen-child-milfam/template-parts/staff/facilitators.php
+++ wp-content/themes/twentyseventeen-child-milfam/template-parts/staff/facilitators.php
@@ -1,12 +1,14 @@
 <div class="individual-person">
   <div class="col_one">
+    <div class="individual-person-thumb-wrapper">
     <div class="individual-person-thumb">
-    <?php if (has_post_thumbnail( $post->ID ) ){
-      $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) );
-    ?>
-      <img src="<?php echo $image[0]; ?>" alt="profile image">
-    <?php } ?>
-  </div>
+      <?php if (has_post_thumbnail( $post->ID ) ){
+        $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) );
+      ?>
+        <img src="<?php echo $image[0]; ?>" alt="profile image">
+      <?php } ?>
+    </div>
+    </div>
     <h3 class="person-name"><?php the_title(); ?><?php echo do_shortcode("[credentials]"); ?></h3>
   </div>
   <div class="col_two">
