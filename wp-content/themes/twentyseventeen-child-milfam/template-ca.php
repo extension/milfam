ce5da785754823d7e2e24fb3b7d1310bd81405f2
Ben MacNeill <ben.macneill@gmail.com>
2017-07-19 17:26:43 -0400

    more landing page design

--- wp-content/themes/twentyseventeen-child-milfam/template-ca.php
+++ wp-content/themes/twentyseventeen-child-milfam/template-ca.php
@@ -5,8 +5,8 @@
 
 get_header(); ?>
 
-<div class="wrap">
-	<div id="primary" class="content-area ca-landing-page">
+<div class="wrap ca-landing-page">
+	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<?php
 			while ( have_posts() ) : the_post();
