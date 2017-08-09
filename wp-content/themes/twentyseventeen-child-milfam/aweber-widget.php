85e0b8b17313e8bc409d57ee3b5dd07123c7b798
Ben MacNeill <ben.macneill@gmail.com>
2017-08-09 13:13:05 -0400

    aweber shortcode

---
+++ wp-content/themes/twentyseventeen-child-milfam/aweber-widget.php
@@ -0,0 +1,15 @@
+<!-- shortcode format:
+[aweber_signup key="1899348052"]
+-->
+
+<div class="aweber-wrapper">
+<div class="AW-Form-<?php echo $a['key'] ?>"></div>
+<script type="text/javascript">(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//forms.aweber.com/form/52/<?php echo $a['key'] ?>.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, "script", "aweber-wjs-2lz77ymhi"));
+</script>
+</div>
