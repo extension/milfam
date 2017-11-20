dbaffff43575ffab8ea8c42326689222c90283cc
Ben MacNeill <ben.macneill@gmail.com>
2017-11-20 11:37:36 -0500

    link person templates to WP person page

--- wp-content/themes/twentyseventeen-child-milfam/template-parts/staff/facilitators.php
+++ wp-content/themes/twentyseventeen-child-milfam/template-parts/staff/facilitators.php
@@ -2,14 +2,16 @@
   <div class="col_one">
     <div class="individual-person-thumb-wrapper">
     <div class="individual-person-thumb">
+      <a href="<?php echo get_permalink( get_the_ID() ); ?>" class="gallery-link">
       <?php if (has_post_thumbnail( $post->ID ) ){
         $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) );
       ?>
         <img src="<?php echo $image[0]; ?>" alt="profile image">
       <?php } ?>
+    </a>
     </div>
     </div>
-    <h3 class="person-name"><?php the_title(); ?><?php echo do_shortcode("[credentials]"); ?></h3>
+    <h3 class="person-name"><a href="<?php echo get_permalink( get_the_ID() ); ?>" class="gallery-link"><?php the_title(); ?><?php echo do_shortcode("[credentials]"); ?></a></h3>
   </div>
   <div class="col_two">
     <?php the_content(); ?>
